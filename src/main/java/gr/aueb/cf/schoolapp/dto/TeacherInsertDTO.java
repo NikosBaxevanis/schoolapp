31b8f481ae60574133b114530aa4fe449c3b8e38
NikosBaxevanis <baxemb@gmail.com>
2025-03-10 21:08:22 +0200

    Initial commit

---
+++ src/main/java/gr/aueb/cf/schoolapp/dto/TeacherInsertDTO.java
@@ -0,0 +1,18 @@
+package gr.aueb.cf.schoolapp.dto;
+
+public class TeacherInsertDTO  extends BaseDTO{
+
+    public TeacherInsertDTO() {
+
+    }
+
+    public TeacherInsertDTO(String firstname, String lastname, String vat,
+                            String fathername,String phoneNum, String email,
+                            String street, String streetNum, String zipCode, Integer cityId) {
+        super(firstname, lastname, vat, fathername, phoneNum, email, street, streetNum, zipCode, cityId);
+    }
+
+
+}
+
+
